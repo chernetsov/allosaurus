9aadc5d5765d5be2af3444925abb06a8324dfa55
Mikhail Chernetsov <chernetsov@gmail.com>
2023-10-09 16:53:22 -0500

    fix version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
    install_requires=[
       'scipy==1.10.1',
       'numpy==1.24.4',
-      'resampy=0.4.2',
+      'resampy==0.4.2',
       'panphon==0.20.0',
       'torch==2.0.1',
       'editdistance==0.6.2',
